4b49ab238040d565c68756ae2cbedaff2a25380d
xienng <741691740@qq.com>
2024-07-09 10:53:23 +0800

    初始化代码提交

---
+++ core/core-backend/src/main/java/io/datagpt/web/chat/request/ClassificationChatRequest.java
@@ -0,0 +1,24 @@
+package io.datagpt.web.chat.request;
+
+import io.datagpt.core.ConversationType;
+
+import java.util.List;
+
+/**
+ * [分类对话请求]
+ * <p>
+ * 用于更加精确的对话请求
+ * <p>
+ *
+ *
+ * @author xienng
+ * @version 1.0
+ * @date 2024年06月30日 16:08
+ */
+public class ClassificationChatRequest {
+    private ConversationType type;
+    private List<Long> knowledgeBaseIds;
+    private List<String> messages;
+    private  String  fileSessionId;
+
+}
---
+++ core/core-backend/src/main/java/io/datagpt/web/common/request/BaseRequest.java
@@ -0,0 +1,32 @@
+package io.datagpt.web.common.request;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+/**
+ * [简要描述类用途]
+ * <p>
+ * [详细描述类用途、功能等，可选]
+ * <p>
+ * [额外的细节描述，比如类的组成、原理等，可选]
+ *
+ * @author xienng
+ * @version 1.0
+ * @date 2024年06月30日 15:51
+ */
+@Data
+public class BaseRequest {
+
+    @Schema(description = "唯一请求id")
+    private String requestId;
+
+    @Schema(description = "请求签名类型", hidden = true)
+    private String signType;
+
+    @Schema(description = "请求时间戳", hidden = true)
+    private Long timeStamp;
+
+    @Schema(description = "请求签名", hidden = true)
+    private String signature;
+
+}
